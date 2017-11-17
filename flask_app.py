b47a7ef17352f990bfcbc8ff1cf9cb890067ba96
Chad Parmet <cparmet@gmail.com>
2017-11-17 12:26:19 +0000

    trying to get POST working

--- flask_app.py
+++ flask_app.py
@@ -2,12 +2,20 @@
 # A very simple Flask Hello World app for you to get started with...
 # https://blog.pythonanywhere.com/121/
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route("/")
-def index():
-    return render_template("main_page.html")
+comments = []
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    # And here's how a POST will be handled.
+    comments.append(request.form["contents"])
+
+    # This tells browser "Please request this page again, this time using a 'GET' method", so that the user can see the results of their post
+    return redirect(url_for('index'))
