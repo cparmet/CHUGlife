7b26579d626b3da3182e7966b56003647cd99f84
Chad <cparmet@gmail.com>
2018-05-31 18:37:41 -0400

    Update column names in data files

--- back_end.py
+++ back_end.py
@@ -124,70 +124,69 @@ def update_cache_with_current_posts(cache_df):
 
     return merged
 
-def patch_captions_that_are_NAN_or_anomaly(data_df_unpatched,comments):
-    '''
-    This function fixes captions that are entered differently on Instagram.
-    It uploads patches from an XLS file (CSV was finnicky) and applies them.
-    '''
-
-    data_df_patched = data_df_unpatched.copy()
-
-    # Import my patches
-    Anomaly_fix = pd.read_excel('/home/cparmet/chuglife/patches.xls', header=0)
-
-    # Force column names to import exactly as I want to refer to them. May no longer be a problem, but was an issue with CSVs.
-    Anomaly_fix.columns = ['img_name', 'caption']
-    Anomaly_fix.set_index('img_name', inplace=True)
-
-    # Iterate through rows in the patch table (CSV)
-    # Remember, about these indices:
-    # - In patch table, img_name is only the jpg name, to fix an issue with IG servers changing.
-    # - In IG_download, indices are sequential integers that will change as we post more photos.
-
-    for i in Anomaly_fix.index:
-
-        # Find the indices in downloaded IG data that match the rows I have in Anomaly_fix csvIG_match_for_this_patch_row = data_df_patched.loc[data_df_patched['img_name'] == i]
-
-        # IG_match_for_this_patch_row = data_df_patched.loc[data_df_patched['img_name'] == i]
-
-        matches=0
-        for j in data_df_patched.index:
-            if i in data_df_patched.ix[j, 'img_name']:
-                index_of_IG_data_to_patch = j
-                matches+=1 # If there's more than 1 match, this 'for' loop will only save the last match. But this counter will be >1.
-
-        ########
-        # Soft QC. Use this opportunity for extra QC of the downloaded IG data.
-        # All img_name from the Anomaly_fix CSV should be in downloaded IG data
-        if matches == 0:
-            comments.append('Warning: A post in patch CSV was not downloaded from IG.')
-            continue
-
-        if matches > 1:  # Each img_name should only occur once
-            comments.append("Error: I have duplicate posts in downloads. Should've been de-duped by now.")
-            # comments.append(IG_match_for_this_patch_row)
-            # Just keep the first one. Cuz really, all that matters in this kind of a database is that it's there once.
-            # IG_match_for_this_patch_row = IG_match_for_this_patch_row.head(1)
-
-            # At this point, we've ensured IG_match_for_this_patch_row is only 1 match.
-        ########
-
-        data_df_patched.ix[index_of_IG_data_to_patch, 'caption'] = Anomaly_fix.ix[i, 'caption']
-
-    return data_df_patched, comments
+# def patch_captions_that_are_NAN_or_anomaly(data_df_unpatched,comments):
+#     '''
+#     This function fixes captions that are entered differently on Instagram.
+#     It uploads patches from an XLS file (CSV was finnicky) and applies them.
+#     '''
+#
+#     data_df_patched = data_df_unpatched.copy()
+#
+#     # Import my patches
+#     Anomaly_fix = pd.read_excel('/home/cparmet/chuglife/patches.xls', header=0)
+#
+#     # Force column names to import exactly as I want to refer to them. May no longer be a problem, but was an issue with CSVs.
+#     Anomaly_fix.columns = ['img_name', 'comment']
+#     Anomaly_fix.set_index('img_name', inplace=True)
+#
+#     # Iterate through rows in the patch table (CSV)
+#     # Remember, about these indices:
+#     # - In patch table, img_name is only the jpg name, to fix an issue with IG servers changing.
+#     # - In IG_download, indices are sequential integers that will change as we post more photos.
+#
+#     for i in Anomaly_fix.index:
+#
+#         # Find the indices in downloaded IG data that match the rows I have in Anomaly_fix csvIG_match_for_this_patch_row = data_df_patched.loc[data_df_patched['img_name'] == i]
+#
+#         # IG_match_for_this_patch_row = data_df_patched.loc[data_df_patched['img_name'] == i]
+#
+#         matches=0
+#         for j in data_df_patched.index:
+#             if i in data_df_patched.ix[j, 'img_name']:
+#                 index_of_IG_data_to_patch = j
+#                 matches+=1 # If there's more than 1 match, this 'for' loop will only save the last match. But this counter will be >1.
+#
+#         ########
+#         # Soft QC. Use this opportunity for extra QC of the downloaded IG data.
+#         # All img_name from the Anomaly_fix CSV should be in downloaded IG data
+#         if matches == 0:
+#             comments.append('Warning: A post in patch CSV was not downloaded from IG.')
+#             continue
+#
+#         if matches > 1:  # Each img_name should only occur once
+#             comments.append("Error: I have duplicate posts in downloads. Should've been de-duped by now.")
+#             # comments.append(IG_match_for_this_patch_row)
+#             # Just keep the first one. Cuz really, all that matters in this kind of a database is that it's there once.
+#             # IG_match_for_this_patch_row = IG_match_for_this_patch_row.head(1)
+#
+#             # At this point, we've ensured IG_match_for_this_patch_row is only 1 match.
+#         ########
+#
+#         data_df_patched.ix[index_of_IG_data_to_patch, 'comment'] = Anomaly_fix.ix[i, 'comment']
+#
+#     return data_df_patched, comments
 
 
 def check_for_null_captions(posts_df,comments):
     ''' QC: any null captions remain? Then give a soft warning'''
 
-    null_captions_df = posts_df.ix[posts_df.caption.isnull(), 'img_name']
+    null_captions_df = posts_df.ix[posts_df.comment.isnull(), 'img_name']
 
     if len(null_captions_df) != 0:
-        comments.append('Warning: Null captions:')
+        comments.append('Warning, tell Chaddy: Null captions...')
         comments.append(list(null_captions_df))
 
     return comments
-
 
 
 def CHUG_it(search_term,comments):
@@ -210,7 +209,7 @@ def CHUG_it(search_term,comments):
     j = 0
 
     for i in posts_df.index:
-        this_post_caption=posts_df.ix[i, 'caption']
+        this_post_caption=posts_df.ix[i, 'comment']
 
         if search_term in str(this_post_caption).lower(): # Make them both lowercase for the match, so it's case-insensitive
             IG_links.append(posts_df.ix[i, 'display_url'])
